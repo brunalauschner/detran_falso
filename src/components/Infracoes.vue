98325aa18ed68a07a9862faf1f3066253592eeb2
brunalauschner <brunalauschner@gmail.com>
2020-05-04 14:07:19 -0300

    Style novo

--- src/components/Infracoes.vue
+++ src/components/Infracoes.vue
@@ -3,21 +3,21 @@
 
       <div id="caixaTotal" v-for="(infracao, index) in infracoes" :key="index">
 
-        <label for="placa">Placa: </label>
+        <label class="letras" for="placa">Placa: </label>
         <input type="text" id="placaq" :value="infracao.PLACA" /><br>
         
-        <label for="placa">Local: </label>
+        <label class="letras" for="placa">Local: </label>
         <input type="text" id="localq" :value="infracao.LOCALIDADE" /><br>
 
-        <h3>Dados da Infração:</h3>
+        <h3 id="tit">Dados da Infração:</h3>
 
-        <label for="data">Tipo:</label>
+        <label class="letras" for="data">Tipo:</label>
         <input type="text" id="tipoq" :value="infracao.DADOS_INFRACAO.TIPO" /><br>
         
-        <label for="placa">Data e Hora:</label>
+        <label class="letras" for="placa">Data e Hora:</label>
         <input type="text" id="datahoraq" :value="infracao.DADOS_INFRACAO.DATAHORA" /><br>
         
-        <label for="placa" id="infracaoq" v-if="infracao.DADOS_INFRACAO.TIPO == 'VELOCIDADE'">Velocidade Máxima: {{infracao.DADOS_INFRACAO.VEL_MAXIMA}} Km/h</label><br>
+        <label class="letras" for="placa" id="infracaoq" v-if="infracao.DADOS_INFRACAO.TIPO == 'VELOCIDADE'">Velocidade Máxima: {{infracao.DADOS_INFRACAO.VEL_MAXIMA}} Km/h</label><br>
 
         <button id="botaoq" @click="voltar"> Nova Consulta </button>
       </div>
@@ -52,9 +52,28 @@ export default {
 
 #caixaTotal{
     border: 1px solid;
-    
+    background-color: rgb(231, 224, 162);
+    text-align: center;
+    width: 45%;
+    height: 250px;
+    margin: 100px 0px 0px 370px;
+    padding: 30px 0px 0px 0px;
 }
-
+.letras{
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 15px;
+  margin: 0px 10px 0px 0px;
+}
+#tit{
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+#botaoq{
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+   margin: 20px 0px 0px 0px;
+   width: 120px;
+   height: 30px;
+   background-color: rgb(209, 198, 101);
+}
 </style>
 
 
--- src/components/Pesquisa.vue
+++ src/components/Pesquisa.vue
@@ -1,9 +1,9 @@
 <template>
-  <fieldset>
-    <legend>Dados do veiculos</legend>
-    <label for="placa">Placa:</label>
-    <input type="text" id="placa" placeholder="informe a placa" maxlength="8" v-model="placa" />
-    <button @click="pesquisarVeiculo">OK</button>
+  <fieldset id="caixa">
+    <legend id="titulo">Dados do veículo</legend>
+    <label  for="placa" id="plac">Placa:</label>
+    <input type="text" id="placa" placeholder=" Escreva aqui" maxlength="8" v-model="placa" />
+    <button id="butao" @click="pesquisarVeiculo">OK</button>
   </fieldset>
 </template>
 
@@ -22,3 +22,31 @@ export default {
   }
 };
 </script>
+
+<style>
+#titulo{
+  width: 180px;
+  height: 30px;
+  text-align: center;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 20px;
+  background-color: rgb(209, 198, 101);
+}
+#caixa{
+   width: 40%;
+   height: 55px;
+   margin: 100px 0px 0px 375px;
+   background-color: rgb(231, 224, 162);
+   text-align: center;
+   padding: 30px;
+}
+#plac{
+   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+   margin: 0px 10px 0px 0px;
+}
+#butao{
+   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+   margin: 0px 0px 0px 5px;
+   background-color: rgb(209, 198, 101);
+}
+</style>
